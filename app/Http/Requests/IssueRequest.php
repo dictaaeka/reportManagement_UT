1178bee4903576482176b2cb17a9197d54412145
dictaaeka <dictaaeka@gmail.com>
2026-08-20 15:43:30 +0800

    fix issue req

--- app/Http/Requests/IssueRequest.php
+++ app/Http/Requests/IssueRequest.php
@@ -1,12 +1,27 @@
-public function rules(): array
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IssueRequest extends FormRequest
 {
-    return [
-        'name' => [
-            'required',
-            'string',
-            'max:255',
-            'unique:issues,name,' . $this->route('issue')?->id,
-        ],
-        'description' => ['nullable', 'string', 'max:1000'],
-    ];
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:issues,name,' . $this->route('issue')?->id,
+            ],
+            'description' => ['nullable', 'string', 'max:1000'],
+        ];
+    }
 }
+
